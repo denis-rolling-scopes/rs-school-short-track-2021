07ec44f0eee194689eb671b5f33aa97793835099
Dzianis Hrynko <d.hrynko@godeltech.com>
2021-04-11 22:12:18 +0300

    feat: 09

--- src/09-sum-digits.js
+++ src/09-sum-digits.js
@@ -10,8 +10,14 @@
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(n) {
+  const arr = n.toString().split('');
+  let sum = 0;
+  arr.forEach((item) => { sum += Number(item); });
+  if (sum >= 10) {
+    return getSumOfDigits(sum);
+  }
+  return sum;
 }
 
 module.exports = getSumOfDigits;
